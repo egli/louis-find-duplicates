58d92e3426bb5ae912fd20778762ab208bddcefe
Christian Egli <christian.egli@sbs.ch>
2012-10-18 16:20:36 +0200

    Define a predicate to test for multiple items

--- src/louis_find_duplicates/core.clj
+++ src/louis_find_duplicates/core.clj
@@ -1,19 +1,22 @@
 (ns louis-find-duplicates.core
   (:gen-class :main true))
 
+(defn multiple? [xs]
+  (< 1 (count xs)))
+
 (defn get-always-opcodes [table]
   (map (comp (partial zipmap [:word :braille]) next) 
        (re-seq #"(?m)^always (\p{L}+) ([0-9-]+)" (slurp table))))
 
 (defn duplicates [table]
   (for [[word opcodes] (group-by :word (get-always-opcodes table))
-        :when (< 1 (count opcodes))] 
+        :when (multiple? opcodes)] 
     [word (set (map :braille opcodes))]))
 
 (defn -main [& args]
   (doseq [table args
           dups (duplicates table)]
     (let [[word braille] dups]
-      (when (< 1 (count braille))
+      (when (multiple? braille)
         (print "Conflicting Duplicate: "))
       (println (str table ": " word ", " (apply str (interpose \, braille)))))))
